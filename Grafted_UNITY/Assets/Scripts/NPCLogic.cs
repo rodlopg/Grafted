233bf0e0508e08796223c0a739c138e441657204
Rodrigo López <rodlopg@gmail.com>
2025-11-11 18:35:30 -0600

    Added new interface and messages

---
+++ Grafted_UNITY/Assets/Scripts/NPCLogic.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NPCLogic : MonoBehaviour
+{
+    [SerializeField] private LayerMask playerLayer;
+    // Booleanos de estado
+    [SerializeField] private TMP_Text tipBubble;
+    [SerializeField] private GameObject interactionIcon;
+
+    // Posibles comentarios o tips que puede hacer
+    [SerializeField] private string tip;
+    [SerializeField] private bool canInteract;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        tipBubble.text = tip;
+
+        /*
+            if (canInteract)
+            {
+                interactionIcon.SetActive(false);
+                tipBubble.gameObject.SetActive(true);
+            }
+            else
+            {
+                tipBubble.gameObject.SetActive(true);
+            }
+        */
+
+        if (((1 << collision.gameObject.layer) & playerLayer) != 0)
+        {
+            tipBubble.gameObject.SetActive(true);
+        }
+            
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        tipBubble.gameObject.SetActive(false);
+        interactionIcon.SetActive(false);
+        
+    }
+
+}
